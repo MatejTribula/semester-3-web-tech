49fba10917a1ee005c5b2109b1c72cfba02e802d
mattveym <matej.tribula@gmail.com>
2025-10-31 18:40:20 +0100

    created carousel component

---
+++ resources/views/components/carousel.blade.php
@@ -0,0 +1,11 @@
+<div class="carousel">
+          <!-- placeholder for now -->
+        <div class="carousel-img"> 
+          <!-- <img src="#" alt="carousel-image"> -->
+        </div>
+        <div class="carousel-progress">
+          <div class="carousel-dot active"></div>
+          <div class="carousel-dot"></div>
+          <div class="carousel-dot"></div>
+        </div>
+      </div>
