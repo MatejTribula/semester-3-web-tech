37e4600614a69cdeb1667dce1db283eb0849c745
mattveym <matej.tribula@gmail.com>
2025-10-31 19:56:55 +0100

    created game-card component

---
+++ resources/views/components/game-card.blade.php
@@ -0,0 +1,8 @@
+<div class="card">
+    <div class="card-img">
+        {{-- <img src="{{ $image }}" alt="{{ $title }}"> --}}
+    </div>
+    <div class="card-info">
+        <h3>{{ $title }}</h3>
+    </div>
+</div>
