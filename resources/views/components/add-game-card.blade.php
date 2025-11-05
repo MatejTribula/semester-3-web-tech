3e7f10192ce44e1f2bc8adf96298251185b35cb2
Rafal F <rafalfuchs2004@gmail.com>
2025-11-05 20:09:13 +0100

    Changed CSS for the add game card button, width 100, added href there to move into upload

--- resources/views/components/add-game-card.blade.php
+++ resources/views/components/add-game-card.blade.php
@@ -1,8 +1,10 @@
-<div class="card add-game-card">
-    <div class="card-img">
-        <i class="fa-solid fa-plus"></i>
+<a href = "{{ route('publish-new-game') }}">
+    <div class="card add-game-card">
+        <div class="card-img">
+            <i class="fa-solid fa-plus"></i>
+        </div>
+        <div class="card-info">
+            <h3>Add New Game</h3>
+        </div>
     </div>
-    <div class="card-info">
-        <h3>Add New Game</h3>
-    </div>
-</div>
+</a>
