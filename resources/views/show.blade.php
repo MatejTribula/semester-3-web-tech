46cb886fefd4d83ea60af8148f957ce58b51978b
mattveym <matej.tribula@gmail.com>
2025-11-11 21:48:43 +0100

    renamed product blade to show and fixed routing

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,12 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('title', 'Product')
+
+@section('content')
+
+  <!-- Product Page Content -->
+  <section class="card-section">
+    <div class="section-header">
+      <h2>{{ $product->title }}</h2>
+    </div>
     
-</body>
-</html>
+    <div class="container2">
+      <div class="carousel">
+        <div class="carousel-img">
+          <!--image-->
+        </div>
+        <div class="carousel-progress">
+          <div class="carousel-dot active"></div>
+          <div class="carousel-dot"></div>
+          <div class="carousel-dot"></div>
+        </div>
+      </div>
+
+      <!-- Right side - Product information -->
+      <div style="flex: 1">
+        <h4>Example genre</h4>
+        <p>
+          {{ $product->description }}
+        </p>
+
+        <button style="button">Get</button>
+      </div>
+    </div>
+  </section>
+
+  @endsection
