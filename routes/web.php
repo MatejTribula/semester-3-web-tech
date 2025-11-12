3f10ee31465ff5c5ad3b838f98af32eb20b02aa1
mattveym <matej.tribula@gmail.com>
2025-11-12 10:24:50 +0100

    authorized access to sites in web.php is handled

--- routes/web.php
+++ routes/web.php
@@ -22,40 +22,29 @@ Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('auth')->group(function () {
+    Route::get('/create', [ProductController::class, 'create'])->name('create');
+    Route::post('/products', [ProductController::class, 'store'])->name('store');
+
+    Route::get('products/{id}/edit', [ProductController::class, 'edit']);
+    Route::put('/products/{id}', [ProductController::class, 'update'])->name('update');
+
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/favorites', function () {
+        return view('favorites');
+    })->name('favorites');
+
+    Route::get('/my-uploads', function () {
+        return view('my-uploads');
+    })->name('my-uploads');
+
+});
 
 Route::get('/products', [ProductController::class, 'index'])->name('home');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('show');
-
-Route::get('/create', [ProductController::class, 'create'])->name('create');
-Route::post('/products', [ProductController::class, 'store'])->name('store');
-
-Route::get('products/{id}/edit', [ProductController::class, 'edit']);
-Route::put('/products/{id}', [ProductController::class, 'update'])->name('update');
-
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-
-//
-//
-//
-//
-
-//
-//
-//
-//
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->name('dashboard');
-
-Route::get('/favorites', function () {
-    return view('favorites');
-})->name('favorites');
-
-Route::get('/my-uploads', function () {
-    return view('my-uploads');
-})->name('my-uploads');
 
 Route::get('/profile', function () {
     return view('profile');
@@ -69,14 +58,6 @@ Route::get('/library', function () {
     return view('library');
 })->name('library');
 
-// Route::get('/login', function () {
-//     return view('login');
-// })->name('login');
-
-// Route::get('/register', function () {
-//     return view('register');
-// })->name('register');
-
-Route::get('/publish-new-game', function () {
-    return view('publish-new-game');
-})->name('publish-new-game');
+// Route::get('/publish-new-game', function () {
+//     return view('publish-new-game');
+// })->name('publish-new-game');
